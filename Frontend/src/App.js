2481cc397305bc7f592521a8d356291a9d975380
ankitkrjha32 <ankitkrjha32@gmail.com>
2024-08-06 13:00:38 +0530

    starter pack

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className=' flex flex-col justify-center items-center h-[100vh]'>
+      <h2 className=' font-extrabold text-blue-600 capitalize  text-3xl'> hello jee kaise hooo </h2>
+      <h3 className=' text-green-800  font-extrabold text-[4rem] capitalize'> Ankit kumar jha   </h3>
+    
+    </div>
+  );
+}
+
+export default App;
