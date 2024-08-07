507761611057477caa8469d89715652e5f456823
ankitkrjha32 <ankitkrjha32@gmail.com>
2024-08-07 15:08:36 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,37 +1,40 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./utils/db.js"
-import userRoute from "./routes/user.route.js";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+import userRoute from './routes/user.route.js';
+import noteRoutes from '././routes/note.route.js'
 
 
-dotenv.config({});
+
+
+dotenv.config();
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 const corsOptions = {
-    origin:'http://localhost:3000',
-    credentials:true
-}
+  origin: 'http://localhost:3000',
+  credentials: true
+};
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 const PORT = process.env.PORT || 8000;
 
+// APIs
+app.use('/api/v1/user', userRoute);
+app.use('/api/v1/notes', noteRoutes);
 
-// api's
-app.use("/api/v1/user", userRoute);
-
-
-
-
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
